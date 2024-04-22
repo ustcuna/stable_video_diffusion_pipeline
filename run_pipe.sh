228b6091deedb539d2cb3006fb2c4979a743f776
ustcuna <dan1.li@intel.com>
2024-04-22 11:56:11 +0800

    Update ENV_PATH

--- run_pipe.sh
+++ run_pipe.sh
@@ -1,4 +1,4 @@
-ENV_PATH=/home/conda/envs/sdxl_turbo/
+ENV_PATH=/home/conda/envs/svd_xt/
 
 export KMP_BLOCKTIME=1
 export KMP_AFFINITY=granularity=fine,compact,1,0
@@ -8,4 +8,4 @@ export LD_PRELOAD=$ENV_PATH/lib/libiomp5.so
 export LD_PRELOAD=$ENV_PATH/lib/libjemalloc.so:$LD_PRELOAD
 export MALLOC_CONF="oversize_threshold:1,background_thread:true,metadata_thp:auto,dirty_decay_ms:9000000000,muzzy_decay_ms:9000000000"
 
-numactl --localalloc --physcpubind=0-47 python test_pipe.py --bf16
+numactl --localalloc --physcpubind=0-47 python test_pipe.py --model_path "/home/models/stable-video-diffusion-img2vid-xt-1-1/" --bf16
